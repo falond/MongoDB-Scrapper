c53b0e945505b4c353305d59b0069bcfd2a0985a
DESARE GAGO <desaregago@DESAREs-MacBook-Pro.local>
2017-12-03 19:03:07 -0500

    new folders

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 // Dependencies
 var express = require("express");
-// Set Handlebars
 var exphbs = require("express-handlebars");
-//mongo db
-var mongojs = require("mongojs");
-//body parser
+var mongoose = require("mongoose");
+var logger = require("morgan");
 var bodyParser = require("body-parser");
-//request
+var methodOverride = require("method-override");
+
+// Requiring our Note and Article models
+var index = require("./models/index.js");
+
+//Scraping tools
 var request = require("request");
 //honeynut cheeriosssss
 var cheerio = require("cheerio");
-//Method override 
-var methodOverride = require("method-override");
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+
 
 //****************************************************
 //request, not sure if this is needed
@@ -28,20 +34,21 @@ app.set("view engine", "handlebars");
 // Initialize Express
 var app = express();
 //****************************************************
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 //****************************************************
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 //****************************************************
 
+// Serve static content
+app.use(express.static("public"));
 
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
+
+
 
 
 app.use('/static', express.static('public'));
